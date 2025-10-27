2e7d861ef93ee0ab1f6193cfcbee6768d95138fd
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-10-27 18:50:49 -0500

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
-    provideRouter(routes, { initialNavigation: 'enabledBlocking' })
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import { Routes } from '@angular/router';
-import { ProductsListComponent } from './pages/products/products-list.component';
-import { ClientsListComponent } from './pages/clients/clients-list.component';
-import { OrdersComponent } from './pages/orders/orders.component';
-import { AccessoriesComponent } from './pages/accessories/accessories.component';
-import { CartComponent } from './pages/cart/cart.component';
+import { ListComponent } from './pages/clientes/list/list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'productos', pathMatch: 'full' },
-  { path: 'productos', component: ProductsListComponent },
-  { path: 'clientes', component: ClientsListComponent },
-  { path: 'pedidos', component: OrdersComponent },
-  { path: 'accesorios', component: AccessoriesComponent },
-  { path: 'carrito', component: CartComponent },
-  { path: '**', redirectTo: 'productos' }
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+  { path: 'clientes', component: ListComponent },
 ];
