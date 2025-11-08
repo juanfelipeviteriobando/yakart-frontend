d40253746723e2a168e7f43f0944767ca8003281
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-11-07 20:10:43 -0500

    version de sakai(cambiar laversion de angular a la correspondiente

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { appConfig } from './app/app.config';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { appConfig } from './app.config';
+import { AppComponent } from './app.component';
 
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    ...(appConfig.providers || []),  // preserva los providers existentes
-    provideHttpClient(),             // habilita HttpClient
-    importProvidersFrom(FormsModule),// habilita FormsModule
-    provideAnimationsAsync(),        // habilita animaciones de PrimeNG
-    providePrimeNG({
-      theme: {
-        preset: Aura                // aplica el tema Aura
-      }
-    })
-  ]
-})
-.catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
